42e98b5c4a952cdd236611c49430b3e6728db8f2
Edward Li <radioactivehydra@gmail.com>
2024-02-18 00:25:04 -0800

    Change turn logic

--- ai_pipeline/move_process.py
+++ ai_pipeline/move_process.py
@@ -41,7 +41,7 @@ def get_command_and_movement(text):
             Given the following string of text, produce a JSON object. The JSON object should be formatted as {"command": command, "radians"?: radians_number, "metres"?: metres: metres_number} where metres is chosen if we select a MOVE command 
     versus radians being chosen if we select a TURN command. You MUST include either radians (turning) or metres (moving). The command value should be
     one of the following commands: ['MOVE_FORWARD', 'MOVE_BACKWARD', 'TURN_LEFT', 'TURN_RIGHT', 'TURN_UP', 'TURN_DOWN']. The metres or radians should be what the user specifies.
-    However, if the user doesn't specify a number (e.g. they say "turn right a little bit"), use your judgement to determine a suitable amount for the output (e.g. a little is 0.5 radians, left turn is 2 radians, a lot is 4 radians, full circle is 8 radians)
+    However, if the user doesn't specify a number (e.g. they say "turn right a little bit"), use your judgement to determine a suitable amount for the output (e.g. a little is 0.5 radians, left turn is 1.7 radians, a half circle is 3.2 radians, full circle is 6.4 radians)
             """
 
     print(prompt)
