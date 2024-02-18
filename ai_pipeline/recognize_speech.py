4b80673c7ee1031ba3e1508f1d61cc4bb70b75a1
Darryl Tanzil <darryltanzil@gmail.com>
2024-02-17 20:09:48 -0800

    modified recognized speech & requirements library

--- ai_pipeline/recognize_speech.py
+++ ai_pipeline/recognize_speech.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 
-def listen_for_keyword():
+def listen_for_keyword(voice_callback):
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
 
@@ -13,39 +13,19 @@ def listen_for_keyword():
         print("Processing audio...")
         # Use Google Web Speech API for speech recognition
         command = recognizer.recognize_google(audio)
-        if "hey spot" in command.lower():
+        if command.startswith("hey spot"):
             # Once the keyword is detected, continue listening for the command
             print("Keyword 'hey spot' detected.")
-            listen_for_command()
+            voice_callback(command[len("hey spot "):])
         else:
             print("Keyword not detected. Listening again.")
-            listen_for_keyword()
+            listen_for_keyword(voice_callback)
     except sr.UnknownValueError:
         print("Sorry, I could not understand the audio.")
-        listen_for_keyword()
+        listen_for_keyword(voice_callback)
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        listen_for_keyword()
-
-def listen_for_command(voice_callback):
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-
-    with microphone as source:
-        print("Listening for command...")
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
-
-    try:
-        print("Processing command...")
-        command = recognizer.recognize_google(audio)
-        voice_callback(command)
-    except sr.UnknownValueError:
-        print("Sorry, I could not understand the command.")
-        listen_for_command()
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        listen_for_command()
+        listen_for_keyword(voice_callback)
 
 if __name__ == "__main__":
-    listen_for_keyword()
+    listen_for_keyword(print)
